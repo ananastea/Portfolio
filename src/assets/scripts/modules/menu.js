0e72af78b3d190d08d4634bd0b41d06ba2a99c88
ananastea <43419371+ananastea@users.noreply.github.com>
2019-01-29 08:07:13 +0300

    Update menu.js

--- src/assets/scripts/modules/menu.js
+++ src/assets/scripts/modules/menu.js
@@ -3,11 +3,20 @@
   const menu = document.getElementById('js_menu');
   const body = document.body;
 
-  btn.addEventListener('click', function() {
-    btn.classList.toggle('burger-btn--active');
-    menu.classList.toggle('nav--active');
-
-    body.classList.toggle('noscroll')
+  const menuPromise = new Promise(function (resolve) {
+    if (btn) {
+      resolve();
+    }
   });
 
+  menuPromise.then(function () {
+
+    btn.addEventListener('click', function () {
+      btn.classList.toggle('burger-btn--active');
+      menu.classList.toggle('nav--active');
+
+      body.classList.toggle('noscroll')
+    });
+
+  });
 })();
