373625a85d6be8e3f51c95302fd5fb1416a61e56
ananastea <43419371+ananastea@users.noreply.github.com>
2019-01-29 08:04:58 +0300

    Update works.js

--- src/assets/scripts/works.js
+++ src/assets/scripts/works.js
@@ -1,3 +1,7 @@
-import './modules/slider'
+import './modules/preloader'
 import './modules/menu'
 import './modules/heroParallax'
+import './modules/slider'
+import './modules/blur'
+import './modules/ajaxCallback'
+import './modules/arrows'
